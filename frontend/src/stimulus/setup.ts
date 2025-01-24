3c0553d5a48cad79dfe0608bf1cff730e292cb23
Christophe Bliard <c.bliard@openproject.com>
2025-01-24 17:10:28 +0100

    Preload DatePicker PreviewController to make tests pass
    When in tests the date picker is opened to click on a day of the
    flatpickr, the controller is not loaded yet and some events are missed,
    making the test fail.
    
    The controller is not ready yet because it needs to be loaded
    dynamically by the OpApplicationController. By registering it in the
    setup.ts, it is loaded before the date picker modal is opened, and the
    controller is connected before a day is clicked, making (hopefully) the
    test pass.

--- frontend/src/stimulus/setup.ts
+++ frontend/src/stimulus/setup.ts
@@ -14,6 +14,7 @@ import OpShowWhenValueSelectedController from './controllers/show-when-value-sel
 import FlashController from './controllers/flash.controller';
 import OpProjectsZenModeController from './controllers/dynamic/projects/zen-mode.controller';
 import PasswordConfirmationDialogController from './controllers/password-confirmation-dialog.controller';
+import PreviewController from './controllers/dynamic/work-packages/date-picker/preview.controller';
 
 declare global {
   interface Window {
@@ -43,3 +44,4 @@ instance.register('show-when-checked', OpShowWhenCheckedController);
 instance.register('show-when-value-selected', OpShowWhenValueSelectedController);
 instance.register('table-highlighting', TableHighlightingController);
 instance.register('projects-zen-mode', OpProjectsZenModeController);
+instance.register('work-packages--date-picker--preview', PreviewController);
